1126d9ba53735a4612c915178274f96d59f93e36
Rihan Mohammed <85993821+rihanmohammed26@users.noreply.github.com>
2022-02-01 01:34:01 +0530

    bug fix

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ module.exports = {
   images: {
     domains: ["i.ibb.co", "upload.wikimedia.org"],
   },
+  env: {
+    API_KEY: "AIzaSyCKmU8AN7OKa-BK5za0U6R-o_KQ8RRONH4",
+    CONTEXT_KEY: "0ab956e2799ff04be",
+  },
 };
